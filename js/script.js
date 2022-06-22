004a99827922fd1fd268fce225c61727a49ba83a
Gustavo Cavaretto <gustavocavaretto@gmail.com>
2022-06-22 10:28:37 +0900

    iniciando-js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Criar uma função em JavaScript
+// Formato ES6 baseado em Arrow Function
+
+// função de realiza o cadastro de livros
+const cadastrar = () =>{
+    // Captura o valor digitado no campo titulo
+    let titulo = document.getElementById('titulo').value
+
+    alert(titulo)
+}
+
+// // Forma de função padrão
+// function Cadastrar(){
+
+// }
